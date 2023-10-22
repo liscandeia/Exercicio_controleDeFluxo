f41d8d0f18f93f9b9fc2a85bd87b8c60bf43a5b5
liscandeia <liscandeia523@gmail.com>
2023-10-21 23:59:11 -0300

    Subindo atividade do módulo 10

---
+++ src/Media.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Media {
+    public static void main(String[] args) {
+        media();
+    }
+
+    private static void media() {
+        System.out.println("Qual sua nota?");
+        Scanner n1 = new Scanner(System.in);
+        Scanner n2 = new Scanner(System.in);
+        Scanner n3 = new Scanner(System.in);
+        Scanner n4 = new Scanner(System.in);
+        float nota1 = n1.nextFloat();
+        float nota2 = n2.nextFloat();
+        float nota3 = n3.nextFloat();
+        float nota4 = n4.nextFloat();
+        float mediN = (nota1 + nota2 + nota3 + nota4)/4;
+        if (mediN >= 7){
+            System.out.println("Aprovado");
+        }else if (mediN >= 5 && mediN<= 6){
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
